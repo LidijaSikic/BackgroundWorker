0fbf40eeb10379ba01fe06c9a12964c9df2d9074
Lidija Sikic <ligalic@vsite.hr>
2020-07-17 06:37:42 +0200

    Add project files.

---
+++ BackgroundWorker/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BackgroundWorkerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.bcgWorker = new System.ComponentModel.BackgroundWorker();
+            this.lblPostotak = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(88, 168);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(542, 42);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(88, 75);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(118, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Obrađuj";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(362, 75);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(118, 36);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Dosta, prekini";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // bcgWorker
+            // 
+            this.bcgWorker.WorkerReportsProgress = true;
+            this.bcgWorker.WorkerSupportsCancellation = true;
+            this.bcgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bcgWorker_DoWork);
+            this.bcgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bcgWorker_ProgressChanged);
+            this.bcgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bcgWorker_RunWorkerCompleted);
+            // 
+            // lblPostotak
+            // 
+            this.lblPostotak.AutoSize = true;
+            this.lblPostotak.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPostotak.Location = new System.Drawing.Point(142, 228);
+            this.lblPostotak.Name = "lblPostotak";
+            this.lblPostotak.Size = new System.Drawing.Size(0, 18);
+            this.lblPostotak.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPostotak);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Pozadinska obrada";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.ComponentModel.BackgroundWorker bcgWorker;
+        private System.Windows.Forms.Label lblPostotak;
+    }
+}
+
---
+++ BackgroundWorker/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerApp
+{
+    public partial class Form1 : Form
+    {
+
+        //BackgroundWorker worker;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            bcgWorker.WorkerReportsProgress = true;
+            bcgWorker.WorkerSupportsCancellation = true;           
+        }
+
+        private void bcgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i+=5)
+            {                
+                Thread.Sleep(500);               
+                bcgWorker.ReportProgress(i);
+
+                if (bcgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void bcgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblPostotak.Text = e.ProgressPercentage.ToString() + " %";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bcgWorker.RunWorkerAsync();           
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {          
+            bcgWorker.CancelAsync();          
+            progressBar.Value = 0;
+            lblPostotak.Text = "";          
+        }
+
+        private void bcgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar.Value = 0;
+            lblPostotak.Text = "";
+        }
+    }
+}
